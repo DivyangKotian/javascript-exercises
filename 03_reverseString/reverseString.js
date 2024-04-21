a0fb7763fb93ff99eacb65bc0176f6227eaecc9b
Divyang Kotian <kotian.divyang@gmail.com>
2024-04-21 07:43:54 -0400

    completed the first 4 set of exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,23 @@
-const reverseString = function() {
-
-};
+const reverseString = function (string) {
+    return string.split("").reverse().join("");     // splits on each char, reverses the order and joins each char
+  };                                                                
+  
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+/*const reverseString = function(word) {
+    let reverseArray= word.split('');       // splitting the string 
+    let lengthOfArray= word.length;         // to find the last index of new array  
+    let newString="";       
+    for(i=lengthOfArray-1; i>=0; i--)       // revesing the string
+        {
+            newString+= reverseArray[i];      // joining the string  
+        }
+        return newString;
+    }; 
+    
+    First attempt commented for note taking
+
+    */
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...item) {
+    return array.filter(a => !(item.includes(a)))           // we are filtering items which are included in the arguemtns (...item) 
 };
 
 // Do not edit below this line
